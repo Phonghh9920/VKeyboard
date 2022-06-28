b3bf896e2d99661b15127948f5db5635c07bc8a6
vladgba <vlad.gba@gmail.com>
2022-06-28 21:44:40 +0300

    Instructions for enabling in Main activity

--- app/src/main/java/com/vladgba/keyb/Main.java
+++ app/src/main/java/com/vladgba/keyb/Main.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main extends Activity {
     @Override
@@ -35,11 +36,12 @@ public class Main extends Activity {
         final Button inputMethod = (Button) findViewById(R.id.main_choose_input_method_btn);
         inputMethod.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                InputMethodManager mgr = null;
                 if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-                    mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    mgr.showInputMethodPicker();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Need Android 6+", Toast.LENGTH_LONG).show();
                 }
-                mgr.showInputMethodPicker();
             }
         });
 
