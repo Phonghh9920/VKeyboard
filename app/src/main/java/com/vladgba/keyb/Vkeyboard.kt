3ff620034d2064983c4472e739eda65199daa9df
vladgba <vlad.gba@gmail.com>
2023-03-24 20:02:05 +0200

    welcome screen

--- app/src/main/java/com/vladgba/keyb/Vkeyboard.kt
+++ app/src/main/java/com/vladgba/keyb/Vkeyboard.kt
@@ -1,57 +1,73 @@
 package com.vladgba.keyb
 
-import android.Manifest
-import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.*
-import android.text.Html
-import android.text.method.LinkMovementMethod
+import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
-import android.widget.*
+import android.widget.Button
 
 class Vkeyboard : Activity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
-        val html = getString(R.string.main_text)
-        val content = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY)
-        } else {
-            Html.fromHtml(html)
-        }
-        val description = findViewById<TextView>(R.id.main_description)
-        description.movementMethod = LinkMovementMethod.getInstance()
-        description.setText(content, TextView.BufferType.SPANNABLE)
 
+        setContentView(R.layout.welcome)
         val manageKeyb = findViewById<Button>(R.id.main_manage_onscreen_keyboards)
+        val inputMethod = findViewById<Button>(R.id.main_choose_input_method_btn)
+
+
         manageKeyb.setOnClickListener {
             startActivityForResult(Intent(android.provider.Settings.ACTION_INPUT_METHOD_SETTINGS), 0)
         }
 
-        val inputMethod = findViewById<Button>(R.id.main_choose_input_method_btn)
-        inputMethod.setOnClickListener {(getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager).showInputMethodPicker()}
+        inputMethod.setOnClickListener {
+            (getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager).showInputMethodPicker()
+        }
 
-        val grantPermissions = findViewById<Button>(R.id.main_grant_permission)
-        grantPermissions.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                if (!Environment.isExternalStorageManager()) {
-                    val intent = Intent(android.provider.Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
-                    val uri: Uri = Uri.fromParts("package", packageName, null)
-                    intent.data = uri
-                    startActivity(intent)
-                }
-            } else {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    requestPermissions(arrayOf(WRITE_EXTERNAL_STORAGE), 1)
+        val settings = findViewById<Button>(R.id.main_settings_btn)
+        settings.setOnClickListener {
+            startActivityForResult(Intent(this, LayoutSettings::class.java), 0)
+        }
+
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            val manageKeyb = findViewById<Button>(R.id.main_manage_onscreen_keyboards)
+            val inputMethod = findViewById<Button>(R.id.main_choose_input_method_btn)
+            val check_mark = getDrawable(android.R.drawable.checkbox_on_background)
+
+            val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            val inputMethodList = inputMethodManager.enabledInputMethodList
+
+            var enabled = false
+            for (inputMethodInfo in inputMethodList) {
+                if (inputMethodInfo.packageName == this.packageName) {
+                    enabled = true
+                    break
                 }
             }
+            manageKeyb.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                if (enabled) check_mark else null,
+                null,
+                null,
+                null
+            )
+
+            val mid = android.provider.Settings.Secure.getString(
+                contentResolver,
+                android.provider.Settings.Secure.DEFAULT_INPUT_METHOD
+            );
+            val imeClass = KeybCtl::class.java
+            val packageName = imeClass.`package`?.name
+            val className = imeClass.simpleName
+            inputMethod.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                if (mid.equals("$packageName/.$className")) check_mark else null,
+                null,
+                null,
+                null
+            )
+
         }
-/*
-        val settings = findViewById<Button>(R.id.main_settings_btn)
-        settings.setOnClickListener { startActivityForResult(Intent(this, Settings::class.java), 0) }
- */
     }
 }
