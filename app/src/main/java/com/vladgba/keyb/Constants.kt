343a09db5826ecf008d0bb0a3506f59787163048
vladgba <vlad.gba@gmail.com>
2023-04-25 20:19:37 +0300

    Settings

---
+++ app/src/main/java/com/vladgba/keyb/Constants.kt
@@ -0,0 +1,107 @@
+package com.vladgba.keyb
+
+internal val LATIN_KEYS = 97..122
+internal const val LATIN_OFFSET = 68
+internal const val MULTIBYTE_UTF = 56320
+internal const val SETTINGS_FILENAME = "settings"
+internal const val NUM_FILENAME = "123"
+internal const val EMOJI_FILENAME = "emoji"
+internal const val LAYOUT_DEFAULT = "en"
+internal const val LAYOUT_EXT = "txt"
+internal const val BLANK_LAYOUT = "blank"
+internal const val LAYOUT_NAME_SUFFIX = ".vkeyb"
+internal const val SETTING_DEBUG = "debug"
+
+// Hardware Key
+internal const val SETTING_REDEFINE_HW_ACTION = "redefineHardwareActions"
+internal const val KEY_DO_IF_HIDDEN = "doIfHidden"
+
+// Key
+internal const val KEY_KEY = "key"
+internal const val KEY_CODE = "code"
+internal const val KEY_HOLD = "hold"
+internal const val KEY_HARD_PRESS = "hard"
+internal const val KEY_TOP_ACTION = "top"
+internal const val KEY_BOTTOM_ACTION = "bottom"
+internal const val KEY_LEFT_ACTION = "left"
+internal const val KEY_RIGHT_ACTION = "right"
+internal const val KEY_TEXT = "text"
+internal const val KEY_TEXT_CURSOR_OFFSET = "textCurOffset"
+internal const val KEY_LAYOUT = "layout"
+
+// Modifier
+internal const val KEY_MOD = "mod"
+internal const val KEY_MOD_META = "meta"
+
+//Mode
+internal const val KEY_MODE = "mode"
+internal const val KEY_MODE_JOY = "joy"
+internal const val KEY_MODE_META = "meta"
+
+// Action
+internal const val KEY_ACTION_SHIFT = "shift"
+internal const val KEY_ACTION_APP = "app"
+internal const val KEY_ACTION_SU = "sudo"
+internal const val KEY_CLIPBOARD = "clipboard"
+internal const val KEY_RECORD = "record"
+internal const val KEY_RANDOM = "rand"
+internal const val KEY_SWITCH_LAYOUT = "switchKeyb"
+
+// Sound
+internal const val KEY_SOUND_PRESS = "soundPress"
+internal const val KEY_SOUND_RELEASE = "soundRelease"
+// TODO: sound (tick, ext)
+internal const val KEY_VIBRATE_TICK = "vibTick"
+internal const val KEY_VIBRATE_PRESS = "vibPress"
+internal const val KEY_VIBRATE_RELEASE = "vibRelease"
+internal const val KEY_VIBRATE_ADDIT = "vibAddit"
+
+// Theme
+internal const val THEME_SWITCH = "themeSwitch"
+internal const val COLOR_DAY = "dayColor"
+internal const val COLOR_NIGHT = "nightColor"
+internal const val COLOR_TEXT_PRIMARY = "primaryTextColor"
+internal const val COLOR_TEXT_PREVIEW = "previewTextColor"
+
+// Style
+internal const val KEY_VISIBLE = "visible"
+internal const val ROW_HEIGHT = "height"
+internal const val KEY_WIDTH = "width"
+internal const val KEY_TEXT_SIZE_PRIMARY = "sizePrimary"
+internal const val KEY_TEXT_SIZE_SECONDARY = "sizeSecondary"
+internal const val KEY_PADDING = "padding"
+internal const val KEY_HIDE_BORDERS = "borderHide"
+internal const val KEY_BORDER_RADIUS = "radius"
+internal const val KEY_SHADOW = "shadow"
+
+// Color
+internal const val COLOR_KEY_BORDER = "keyBorderColor"
+internal const val COLOR_KEYBOARD_BACKGROUND = "keyboardBackgroundColor"
+internal const val COLOR_KEY_STATIC_BG = "bg"
+internal const val COLOR_KEY_BACKGROUND = "keyBackgroundColor"
+internal const val COLOR_KEY_MOD_BACKGROUND = "modBackgroundColor"
+internal const val COLOR_KEY_SHADOW = "shadowColor"
+internal const val COLOR_KEY_PREVIEW_SELECTED = "previewSelectedColor"
+internal const val COLOR_KEY_SECONDARY = "secondaryTextColor"
+
+// Sensitivity
+internal const val SENSE_HARD_PRESS = "hardSense"
+internal const val SENSE_HOLD_PRESS = "longPressTime"
+internal const val SENSE_ADDITIONAL_CHARS = "extSense"
+internal const val SENSE_HORIZONTAL_TICK = "horTick"
+internal const val SENSE_VERTICAL_TICK = "verTick"
+
+// Shift Action
+internal const val KEY_SHIFTING_UPPER_ALL = "upperAll"
+internal const val KEY_SHIFTING_LOWER_ALL = "lowerAll"
+
+// Layout Switch
+internal const val LAYOUT_PREV = "@PREV"
+internal const val LAYOUT_NEXT = "@NEXT"
+internal const val LAYOUT_NUM = "@NUM"
+internal const val LAYOUT_TEXT = "@TEXT"
+internal const val LAYOUT_EMOJI = "@EMOJI"
+
+internal enum class KeybType {
+    NORMAL, NUMBER, PHONE, URI, EMAIL, DATETIME, DATE, TIME
+}
--- app/src/main/java/com/vladgba/keyb/Settings.kt
+++ app/src/main/java/com/vladgba/keyb/Settings.kt
@@ -1,45 +1,44 @@
 package com.vladgba.keyb
 
-class Settings {
-    companion object {
 
-        var all: LayoutParse.DataNode = LayoutParse.DataNode()
-        var lastModified = 0L
-        var padding = 5f
-        var primaryFont = 2f
-        var secondaryFont = 4.5f
-        var horTick = 30
-        var verTick = 50
-        var offset = 70
-        var longPressTime = 300L
-
-        operator fun get(s: String): LayoutParse.DataNode = if (all.has(s)) all[s] else LayoutParse.DataNode()
-        fun has(k: String) = all.has(k)
-
-        operator fun plusAssign(map: LayoutParse.DataNode) {
-            if (all.count() > 0) {
-                // TODO: adding
-            } else {
-                all = map
-            }
-        }
-        fun getVal(s: String, d: String): String {
-            if (!has(s)) return d
-            val r = all.str(s)
-            return r.ifEmpty { d }
-        }
-
-        fun setDefaults() {
-            primaryFont = getVal("sizePrimary", "2").toFloat()
-            secondaryFont = getVal("sizeSecondary", "4.5").toFloat()
-            horTick = getVal("horizontalSense", "30").toInt()
-            verTick = getVal("verticalSense", "50").toInt()
-            offset = getVal("extendedSense", "70").toInt()
-            longPressTime = getVal("longPressMs", "300").toLong()
-        }
-
-        fun str(s: String) = if (all.has(s)) all.str(s) else ""
-
-        fun num(i: String) = if (all.has(i)) all.num(i) else 0
-    }
+object Settings : Flexaml.FxmlNode(Flexaml("""
+{
+   "$SETTING_DEBUG":"0",
+   "$KEY_TEXT_SIZE_PRIMARY":"2.5",
+   "$KEY_TEXT_SIZE_SECONDARY":"5",
+   "$COLOR_DAY":"baseLight",
+   "$COLOR_NIGHT":"baseDark",
+   "baseLight":{
+      "$COLOR_TEXT_PRIMARY":"ff000000",
+      "$COLOR_KEY_SECONDARY":"48000000",
+      "$COLOR_KEYBOARD_BACKGROUND":"ffeeeeee",
+      "$COLOR_KEY_BORDER":"33000000",
+      "$COLOR_KEY_BACKGROUND":"ffffffff",
+      "$COLOR_KEY_PREVIEW_SELECTED":"22000000",
+      "$COLOR_TEXT_PREVIEW":"ff222222",
+      "$COLOR_KEY_SHADOW":"55000000",
+      "$COLOR_KEY_MOD_BACKGROUND":"ff0099ff"
+   },
+   "baseDark":{
+      "$COLOR_TEXT_PRIMARY":"ffffffff",
+      "$COLOR_KEY_SECONDARY":"59ffffff",
+      "$COLOR_KEYBOARD_BACKGROUND":"ff1a1a1a",
+      "$COLOR_KEY_BORDER":"ff4d4d4d",
+      "$COLOR_KEY_BACKGROUND":"ff333333",
+      "$COLOR_KEY_PREVIEW_SELECTED":"ff000000",
+      "$COLOR_TEXT_PREVIEW":"ffffffff",
+      "$COLOR_KEY_SHADOW":"66000000",
+      "$COLOR_KEY_MOD_BACKGROUND":"440099ff"
+   },
+   "$KEY_BORDER_RADIUS":10,
+   "$KEY_PADDING":10,
+   "$SENSE_HARD_PRESS":400,
+   "$SENSE_ADDITIONAL_CHARS":70,
+   "$SENSE_HORIZONTAL_TICK":70,
+   "$SENSE_VERTICAL_TICK":60,
+   "$SENSE_HOLD_PRESS":200,
+   "$KEY_VISIBLE":1
+}
+""").parse()) {
+    var lastModified = 0L
 }
