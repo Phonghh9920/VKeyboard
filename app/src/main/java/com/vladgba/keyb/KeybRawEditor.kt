c7e1cd8d8fd4f1713fee67b27d5f32731fb62144
vladgba <vlad.gba@gmail.com>
2023-04-26 19:33:36 +0300

    add layout editor & raw editor

---
+++ app/src/main/java/com/vladgba/keyb/KeybRawEditor.kt
@@ -0,0 +1,55 @@
+package com.vladgba.keyb
+
+import android.app.Activity
+import android.os.Bundle
+import android.text.Editable
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import android.widget.Toast
+
+
+class KeybRawEditor : Activity() {
+    private var editText: EditText? = null
+    lateinit var name: String
+    var base: String = ""
+    lateinit var file: PFile
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.raw_edit)
+        name = intent.getStringExtra("name") ?: return finish()
+        base = intent.getStringExtra("base") ?: ""
+        title = name
+        file = PFile(this, name)
+        editText = findViewById(R.id.editText)
+        editText!!.width = windowManager.defaultDisplay.width // TODO: deprecated "defaultDisplay"
+        editText!!.text = Editable.Factory.getInstance().newEditable(if (file.exists()) file.read() else loadFromAssets() ?: "")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.raw_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_save -> {
+                Toast.makeText(this, R.string.file_saving, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, if(file.write(editText!!.editableText.toString())) R.string.file_saved else R.string.file_save_fail, Toast.LENGTH_LONG).show()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun loadFromAssets(): String? {
+        return try {
+            assets.open("$name.$LAYOUT_EXT").bufferedReader().use { it.readText() }
+        } catch (ex: Exception) {
+            null
+        }
+    }
+
+
+}
